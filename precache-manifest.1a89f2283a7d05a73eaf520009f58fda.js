894186f0a9b827e39b4319a9255a4d2635699100
sonictruth <dextor@gmail.com>
2018-11-26 13:20:18 +0100

    Updates

---
+++ precache-manifest.1a89f2283a7d05a73eaf520009f58fda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7d3446b52e37155951d",
+    "url": "/ghistory-test/static/js/runtime~main.e7d3446b.js"
+  },
+  {
+    "revision": "6c34630ff45efbc4ab6d",
+    "url": "/ghistory-test/static/js/main.6c34630f.chunk.js"
+  },
+  {
+    "revision": "f787e0371504aef0e0ed",
+    "url": "/ghistory-test/static/js/1.f787e037.chunk.js"
+  },
+  {
+    "revision": "6c34630ff45efbc4ab6d",
+    "url": "/ghistory-test/static/css/main.87a24c74.chunk.css"
+  },
+  {
+    "revision": "d60e4572fd612992b166fbdaf5535f11",
+    "url": "/ghistory-test/index.html"
+  },
+  {
+    "revision": "cf8519c2ba0e41cd18f130ae2e5c06e4",
+    "url": "/ghistory-test/6b13dcba0c14505baba2.worker.js"
+  }
+];
