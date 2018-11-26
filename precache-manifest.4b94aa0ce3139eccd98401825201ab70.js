06842ddc067cd2c69644bd08d275bb9068fbc295
sonictruth <dextor@gmail.com>
2018-11-26 17:06:10 +0100

    Updates

--- precache-manifest.4b94aa0ce3139eccd98401825201ab70.js
+++ precache-manifest.4b94aa0ce3139eccd98401825201ab70.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/ghistory-test/static/js/runtime~main.e7d3446b.js"
   },
   {
-    "revision": "71d226fb606ec95fffee",
-    "url": "/ghistory-test/static/js/main.71d226fb.chunk.js"
+    "revision": "da078d1bc65b79329438",
+    "url": "/ghistory-test/static/js/main.da078d1b.chunk.js"
   },
   {
     "revision": "210f8d487307a4ca0c58",
     "url": "/ghistory-test/static/js/1.210f8d48.chunk.js"
   },
   {
-    "revision": "71d226fb606ec95fffee",
+    "revision": "da078d1bc65b79329438",
     "url": "/ghistory-test/static/css/main.b61a1ea7.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/ghistory-test/static/css/1.afe5fbf6.chunk.css"
   },
   {
-    "revision": "e538b768a832e73de4ed4f32845e43be",
+    "revision": "d7f119cb27abf7073002615c52e5c2e3",
     "url": "/ghistory-test/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ghistory-test/precache-manifest.4286574e230fbd64d253f44e3e755fe4.js"
+  "/ghistory-test/precache-manifest.4b94aa0ce3139eccd98401825201ab70.js"
 );
 
 workbox.clientsClaim();
